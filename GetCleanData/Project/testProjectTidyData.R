321c7b33d9a291ec1b8295fd5208c2433873348e
Michoj <michoj@hotmail.com>
2015-01-11 18:17:29 +0200

    deleted:    GetCleanData/.Rhistory 	deleted:    GetCleanData/Project/CookBook.md 	deleted:    GetCleanData/Project/ProjectTidyData.txt 	deleted:    GetCleanData/Project/README.md 	deleted:    GetCleanData/Project/run_analysis.R 	deleted:    GetCleanData/Project/testProjectTidyData.R 	deleted:    GetCleanData/Work/Quiz3Q1.R 	deleted:    GetCleanData/Work/Quiz3Q2.R 	deleted:    GetCleanData/Work/Quiz3Q3.R 	deleted:    GetCleanData/Work/Quiz3Q4.R 	deleted:    GetCleanData/Work/Quiz3Q5.R 	deleted:    GetCleanData/Work/getdata_data_DATA.gov_NGAP.xlsx 	deleted:    GetCleanData/Work/getdata_data_FEDSTATS_Country.csv 	deleted:    GetCleanData/Work/getdata_data_GDP.csv 	deleted:    GetCleanData/Work/getdata_data_PUMSDataDict06.pdf 	deleted:    GetCleanData/Work/getdata_data_restaurants.xml 	deleted:    GetCleanData/Work/getdata_data_ss06hid.csv 	deleted:    GetCleanData/Work/getdata_data_ss06pid.csv 	deleted:    GetCleanData/Work/getdata_jeff.jpg 	deleted:    GetCleanData/Work/try.xml 	deleted:    GetCleanData/Work/try2.xml 	deleted:    ProgrammingAssignment2

--- GetCleanData/Project/testProjectTidyData.R
+++
@@ -1,8 +0,0 @@
-workingDirectory <- "C:/Coursera/Data Science/Getting and Cleaning Data/Course Project/UCI HAR Dataset"
-file_path <- "ProjectTidyData.txt"
-cat ("\014") # Clear Console
-setwd(workingDirectory)
-# Read data set
-data <- read.table(file_path, header = TRUE)
-# Display a subset
-data[1:20,1:4]
--- GetCleanData/Work/Quiz3Q2.R
+++
@@ -1,7 +0,0 @@
-# cat("\014")  # Clear Console
-library(jpeg)
-fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
-download.file(fileURL, dest = "getdata_jeff.jpg")
-y <- readJPEG("getdata_jeff.jpg", native=TRUE)
-quantile(y)
-quantile(y, probs = c(0.3,0.8))
